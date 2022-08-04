dcc3c74de7f78e33ea04514907a9445c8f8db6c9
Gabriel Lacorte <gabrielslacorte@hotmail.com>
2022-08-03 21:09:41 -0300

    Finalizando js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 function start() {
     var buttonCalculateImc = document.querySelector('#button-calculate-imc');
     buttonCalculateImc.addEventListener('click', handleCalculateImc);  
-    var inputWeight = document.querySelector('#input-weight');
-    var inputHeight = document.querySelector('#input-height'); 
-     
-    handleCalculateImc();
+   // var inputWeight = document.querySelector('#input-weight');
+    //var inputHeight = document.querySelector('#input-height');      
+   // handleCalculateImc();
+   
   }  
   function handleCalculateImc() {
     var inputWeight = Number(document.querySelector('#input-weight').value);
@@ -15,30 +15,33 @@ function start() {
   
     var resultImc = document.querySelector('#imc');
     resultImc.textContent = imc.toFixed(2).replace('.', ',');
+    
+    condicao(imc)
   }
   
   function calculateImc(weight, height) {
     return weight / (height * height);
-  }  
-  start();  
     
-  /*function condicao(){
+  }         
+  function condicao(imc){
+    
+    var mensagemIMC = document.querySelector(`#p-mensagem`)
         if (imc < 18.5){
-        console.log (`Abaixo do peso`)
+        mensagemIMC.textContent = (`Abaixo do peso`)
     } else if (imc > 18.60 && imc < 25.00) {
-        console.log(`Peso ideal (parabéns)`)
-    } else if ( imc >25.00 && imc < 30.00) {
-        console.log (`Levemente acimado peso`)
+        mensagemIMC.textContent = (`Peso ideal (parabéns)`)
+    } else if ( imc > 25.00 && imc < 30.00) {
+        mensagemIMC.textContent = (`Levemente acimado peso`)
     } else if (imc > 30.00 && imc < 35.00){
-        console.log (`Obesidade grau I`)
+        mensagemIMC.textContent = (`Obesidade grau I`)
     } else if (imc > 35 && imc < 40.00) {
-        console.log (`Obesidade grau II (severa)`)
+        mensagemIMC.textContent =  (`Obesidade grau II (severa)`)
     } else {
-        console.log (`Obesidade III(mórbita)`) 
+        mensagemIMC.textContent =  (`Obesidade III(mórbita)`) 
+    } 
     }
-    }
-   */
-  
+    
+    start();  
 
 
 
